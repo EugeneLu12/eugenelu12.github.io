a9c81dc95ea9e9ac884727457cac0b2ce98f7a6d
Eugene Lu <e6lu@edu.uwaterloo.ca>
2019-05-30 12:42:54 -0700

    redesigned UI, updated skills profile

--- Header.js
+++ Header.js
@@ -1,42 +1,19 @@
-window.addEventListener('scroll', function() {
-    let skillsHeaderTopPos = document.getElementById("skillStart").getBoundingClientRect().top;
-    let navBarBottomPos = document.getElementById("navBar").getBoundingClientRect().bottom;
-    let navDiv = document.getElementsByClassName("navBarHeader").item(0);
-    
-    if (skillsHeaderTopPos < navBarBottomPos) {
-        navDiv.style.background = "none";
-        navDiv.style.backgroundColor = "#1F2121";
-        let css = '.navigation a:hover {background-color: #141515; opacity: 1; }';
-        if (shouldDeleteHoverEffect()) {
-            deleteNewHoverEffect();
+document.addEventListener('DOMContentLoaded', function() {
+    navBarSlidingAnimation();
+ }, false);
+
+ function navBarSlidingAnimation() {
+    let navBar = document.getElementsByClassName("navBarHeader").item(0);
+    navBar.style.top = "-100px";
+    let pos = -100;
+    navBar.style.background = "none";
+    navBar.style.backgroundColor = "#141515";
+    let animation = setInterval(function() {
+        if (pos <= 0) {
+            navBar.style.top = pos + "px";
+            pos++;
+        } else {
+            clearInterval(animation);
         }
-        appendNewHoverEffect(css);
-    } else {
-        navDiv.style.backgroundImage = "url('src/img/woodcropped.png')";
-        let css = '.navigation a:hover {opacity: 1; }';
-        if (shouldDeleteHoverEffect()) {
-            deleteNewHoverEffect();
-        }
-        appendNewHoverEffect(css);
-    }
-});
-
-function shouldDeleteHoverEffect() {
-    let lastChild = document.getElementsByTagName('head')[0].lastChild;
-    return lastChild.tagName === "STYLE";
-}
-
-function appendNewHoverEffect(css) {
-    let style = document.createElement('style');
-    if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-    document.getElementsByTagName('head')[0].appendChild(style);
-}
-
-function deleteNewHoverEffect() {
-    let head = document.getElementsByTagName('head')[0];
-    head.removeChild(head.lastChild);
+    }, 5);
 }
