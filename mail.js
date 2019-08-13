b1b8e5630434ddc640e2c4b9fa40f184ec0bd71b
Eugene Lu <e6lu@edu.uwaterloo.ca>
2019-08-13 12:16:48 -0700

    fixed broken mailto link

--- mail.js
+++ mail.js
@@ -7,5 +7,5 @@ function sendEmail() {
     let subject = document.getElementsByName('subject').item(0).value;
     let message = document.getElementsByName('message').item(0).value;
     console.log(name, subject, message)
-    window.location.href = `e6lu@edu.uwaterloo.ca?subject=${subject}&body=${message}`;
+    window.location.href = `mailto:e6lu@edu.uwaterloo.ca?subject=${subject}&body=${message}`;
 }
