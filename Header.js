8a136110634e3e3b55cf5df9d474698bbfc6a690
Eugene Lu <e6lu@edu.uwaterloo.ca>
2019-08-13 11:56:25 -0700

    redesigned website, refactored code

--- Header.js
+++ Header.js
@@ -1,16 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    navBarSlidingAnimation();
+let shouldAnimate = true;
+
+document.addEventListener('scroll', function() {
+    let windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    let navbar = document.getElementsByClassName('navbar').item(0);
+    let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : 
+        (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        if (scrollTop > windowHeight) {
+            if (shouldAnimate) {
+                shouldAnimate = false;
+                navbarSlidingAnimation(navbar);
+            }
+        } else {
+            navbar.style.background ='none';
+            shouldAnimate = true;
+        }
  }, false);
 
- function navBarSlidingAnimation() {
-    let navBar = document.getElementsByClassName("navBarHeader").item(0);
-    navBar.style.top = "-100px";
+ function scrollToElement(element) {
+    document.getElementsByClassName(element).item(0).scrollIntoView(true);
+}
+
+function navbarSlidingAnimation(navbar) {
+    shouldAnimate = false;
+    navbar.style.top = '-100px';
     let pos = -100;
-    navBar.style.background = "none";
-    navBar.style.backgroundColor = "#141515";
+    navbar.style.backgroundColor = '#1f2631';
     let animation = setInterval(function() {
         if (pos <= 0) {
-            navBar.style.top = pos + "px";
+            navbar.style.top = pos + 'px';
             pos++;
         } else {
             clearInterval(animation);
---
+++ mail.js
@@ -0,0 +1,11 @@
+document.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+function sendEmail() {
+    let name = document.getElementsByName('name').item(0).value;
+    let subject = document.getElementsByName('subject').item(0).value;
+    let message = document.getElementsByName('message').item(0).value;
+    console.log(name, subject, message)
+    window.location.href = `mailto:test@example.com?subject=${subject}&body=${message}`;
+}
