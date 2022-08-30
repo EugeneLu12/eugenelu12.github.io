81b3a540bd547f184dbddf58106528287483280e
Eugene Lu <eugenelu1@Hotmail.com>
2022-08-30 12:53:24 -0400

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,6 +1,6 @@
 let introHeader = document.getElementsByClassName('intro__text')[0];
 let cmd = "cat about.txt"
-let aboutMeStr = "<br>Hi, I'm Eugene. I'm software engineer with 2 years of experience. I'm a hardcore Gopher."
+let aboutMeStr = "<br>Hi, I'm Eugene. I'm a software engineer with 2 years of experience. I'm a hardcore Gopher."
 let index = 0;
 
 window.onload = () => {
